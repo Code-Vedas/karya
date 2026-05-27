1c513b4438cac348054dfab1cf1ebda272c73748
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2026-05-27 00:37:04 -0400

    test: enhance dashboard rendering tests

--- gems/karya-sinatra/spec/karya/sinatra_module_spec.rb
+++ gems/karya-sinatra/spec/karya/sinatra_module_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe Karya::Sinatra do
 
   it 'executes support and runtime wrapper methods directly on the framework module' do
     runtime = instance_double(Karya::FrameworkRuntime::HostRuntime, health_payload: { 'status' => 'ok' })
+    allow(Karya::Dashboard).to receive(:render_document).and_return('<html>Karya Dashboard</html>')
     allow(Karya::FrameworkRuntime).to receive(:build).and_call_original
     allow(Karya::FrameworkRuntime).to receive(:with_started_runtime).and_yield(runtime)
     described_class.instance_variable_set(:@support, nil)
@@ -24,7 +25,13 @@ RSpec.describe Karya::Sinatra do
     expect(described_class.support).to be_a(Karya::Internal::FrameworkSupport)
     expect(described_class.build_host_runtime(backend_class:, backend_options: {})).to be_a(Karya::FrameworkRuntime::HostRuntime)
     expect(described_class.with_host_runtime(backend_class:, backend_options: {}, &:health_payload)).to eq('status' => 'ok')
-    expect(described_class.render_dashboard_page).to include('Karya Dashboard')
+    expect(described_class.render_dashboard_page).to eq('<html>Karya Dashboard</html>')
+    expect(Karya::Dashboard).to have_received(:render_document).with(
+      title: Karya::Dashboard::DEFAULT_TITLE,
+      mount_path: '/karya',
+      asset_prefix: nil,
+      name: 'dashboard'
+    )
   ensure
     described_class.instance_variable_set(:@support, nil)
   end
